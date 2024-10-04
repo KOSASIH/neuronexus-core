f2f21877c5d15dce95288a39f9503463f509f569
KOSASIH <kosasihg88@gmail.com>
2024-10-05 00:02:21 +0700

    Create qubit_measurement.py

---
+++ quantum/qubits/qubit_measurement/qubit_measurement.py
@@ -0,0 +1,46 @@
+import numpy as np
+from qiskit import QuantumCircuit, execute, Aer
+
+class QubitMeasurement:
+    def __init__(self):
+        pass
+
+    def measure_z_basis(self, qubit):
+        circuit = QuantumCircuit(1)
+        circuit.measure(qubit, 0)
+        return circuit
+
+    def measure_x_basis(self, qubit):
+        circuit = QuantumCircuit(1)
+        circuit.h(qubit)
+        circuit.measure(qubit, 0)
+        return circuit
+
+    def measure_y_basis(self, qubit):
+        circuit = QuantumCircuit(1)
+        circuit.s(qubit)
+        circuit.h(qubit)
+        circuit.measure(qubit, 0)
+        return circuit
+
+def main():
+    qubit_measurement = QubitMeasurement()
+    measure_z_circuit = qubit_measurement.measure_z_basis(0)
+    measure_x_circuit = qubit_measurement.measure_x_basis(0)
+    measure_y_circuit = qubit_measurement.measure_y_basis(0)
+
+    simulator = Aer.get_backend('qasm_simulator')
+    job = execute(measure_z_circuit, simulator)
+    result = job.result()
+    print(result.get_counts())
+
+    job = execute(measure_x_circuit, simulator)
+    result = job.result()
+    print(result.get_counts())
+
+    job = execute(measure_y_circuit, simulator)
+    result = job.result()
+    print(result.get_counts())
+
+if __name__ == "__main__":
+    main()
