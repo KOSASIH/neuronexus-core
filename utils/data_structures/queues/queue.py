64a46483c10d19733fd4da04fa8552b865cf6f8f
KOSASIH <kosasihg88@gmail.com>
2024-10-05 15:51:04 +0700

    Create queue.py

---
+++ utils/data_structures/queues/queue.py
@@ -0,0 +1,26 @@
+# queue.py
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
