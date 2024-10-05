fdd5e29bc9012cab5a8797a6325facf27f4de8ce
KOSASIH <kosasihg88@gmail.com>
2024-10-05 15:50:15 +0700

    Create stack.py

---
+++ utils/data_structures/stacks/stack.py
@@ -0,0 +1,26 @@
+# stack.py
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
